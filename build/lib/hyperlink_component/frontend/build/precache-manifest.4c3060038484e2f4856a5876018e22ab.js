07ede360de5bac7cd9272e64094a5ecd6e1244e8
Granit Luzhnica <granit.luzhnica@gmail.com>
2022-08-23 19:30:53 +0200

    first commit

---
+++ build/lib/hyperlink_component/frontend/build/precache-manifest.4c3060038484e2f4856a5876018e22ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9206386eeff6a4d804aa5505e2cb4a97",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18ffe1d026a9c417910d",
+    "url": "./static/js/2.17ff5fb8.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.17ff5fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6016188e0b534cbff82",
+    "url": "./static/js/main.08441bcb.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
---
+++ hyperlink_component/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import HyperlinkComponent from "./HyperlinkComponent"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HyperlinkComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
