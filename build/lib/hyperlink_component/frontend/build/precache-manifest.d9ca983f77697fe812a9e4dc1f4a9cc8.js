4ec666e140129c7ed855f07ac37a2e48e9f91696
Granit Luzhnica <granit.luzhnica@gmail.com>
2022-08-23 21:29:44 +0200

    improvments

---
+++ build/lib/hyperlink_component/frontend/build/precache-manifest.d9ca983f77697fe812a9e4dc1f4a9cc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f8aa33d9ecccd00a047ced3d7502b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2de16bf0096a225277db",
+    "url": "./static/js/2.3876b695.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.3876b695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e5ee093b03270ef4f2",
+    "url": "./static/js/main.56fd793c.chunk.js"
+  },
+  {
+    "revision": "0ee13725d89c19f6d392",
+    "url": "./static/js/runtime-main.68d35b50.js"
+  }
+]);
